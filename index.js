93806ac11baf250214ed27fe28b371a79f323b3b
chrischne <chrischne@gmail.com>
2019-08-09 12:38:35 +0200

    initial

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-var data = [{
-    x: 100,
-    y: 200
-},{
-    x: 300,
-    y: 130
-},{
-    x: 120,
-    y: 240
-},{
-    x: 330,
-    y: 210
-}];
+d3.csv('dots.csv',function(d){
+    return {
+        r: +d.val,
+        y: +d.posx,
+        x: +d.posy
+    };
+}).then(function(data){
+    console.log(data);
 
-
-var svg = d3.select("body").append("svg")
+    var svg = d3.select("body").append("svg")
     .attr("width", "800")
     .attr("height", "600");
 
-
-svg.selectAll("circle")
+    svg.selectAll("circle")
     .data(data)
     .join('circle')
     .attr('cx', function (d, i) {
-        return d.x
+        return d.x;
     })
     .attr('cy', function(d,i){
-        return d.y
+        return d.y;
     })
     .attr('r', function (d, i) {
-        return 10;
+        return d.r;
     });
+
+});
