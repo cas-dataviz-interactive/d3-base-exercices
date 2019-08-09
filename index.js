2e780a7b41f96d5c94e6d407fa8c708eda522257
chrischne <chrischne@gmail.com>
2019-08-09 12:46:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-d3.csv('dots.csv',function(d){
+d3.csv('pigeons_short.csv', function (d) {
     return {
-        r: +d.val,
-        y: +d.posx,
-        x: +d.posy
+        id: +d.id,
+        speed: +d.speed,
+        lat: +d.lat,
+        lon: +d.lon
     };
-}).then(function(data){
+}).then(function (data) {
     console.log(data);
 
     var svg = d3.select("body").append("svg")
-    .attr("width", "800")
-    .attr("height", "600");
+        .attr("width", 800)
+        .attr("height", 600);
+
+    var y= d3.scaleLinear()
+        .domain([27, 50])
+        .range([0, 600]);
+
+    var x = d3.scaleLinear()
+        .domain([-120, -80])
+        .range([0, 800]);
+
 
     svg.selectAll("circle")
-    .data(data)
-    .join('circle')
-    .attr('cx', function (d, i) {
-        return d.x;
-    })
-    .attr('cy', function(d,i){
-        return d.y;
-    })
-    .attr('r', function (d, i) {
-        return d.r;
-    });
+        .data(data)
+        .join('circle')
+        .attr('cx', function (d, i) {
+            return x(d.lon);
+        })
+        .attr('cy', function (d, i) {
+            return y(d.lat);
+        })
+        .attr('r', function (d, i) {
+            return 0.3*d.speed;
+        });
 
 });
