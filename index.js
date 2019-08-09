165916ab918d72bff72541f4a0993ccf53196694
chrischne <chrischne@gmail.com>
2019-08-09 12:30:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var data = [5,11,18,22,7];
+var data = [{
+    x: 100,
+    y: 200
+},{
+    x: 300,
+    y: 130
+},{
+    x: 120,
+    y: 240
+},{
+    x: 330,
+    y: 210
+}];
+
 
 var svg = d3.select("body").append("svg")
-    .attr("width",800)
-    .attr("height",600);
+    .attr("width", "800")
+    .attr("height", "600");
 
 
 svg.selectAll("circle")
     .data(data)
     .join('circle')
-    .attr('cx',function(d,i){
-        return 100+ i*100;
+    .attr('cx', function (d, i) {
+        return d.x
     })
-    .attr('cy',100)
-    .attr('r',function(d,i){
-        return d;
+    .attr('cy', function(d,i){
+        return d.y
+    })
+    .attr('r', function (d, i) {
+        return 10;
     });
