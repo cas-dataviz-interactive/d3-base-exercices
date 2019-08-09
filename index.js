2c4bc88c2959a22eff30a8c6011e41fce4e2c434
chrischne <chrischne@gmail.com>
2019-08-09 12:28:08 +0200

    draw rectangles

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+var data = [5,11,18,22,7];
 
 var svg = d3.select("body").append("svg")
-    .attr("width", 800)
-    .attr("height", 600);
+    .attr("width","800")
+    .attr("height","600");
+
+
+svg.selectAll("rect")
+    .data(data)
+    .join('rect')
+    .attr('x',100)
+    .attr('y',function(d,i){
+        return 100+i*20;
+    })
+    .attr('width',function(d,i){
+        return d*20;
+    })
+    .attr('height',10);
+    
