0bde5eb4b39dd971bdea77d9678a99ef30792fd0
chrischne <chrischne@gmail.com>
2019-08-09 12:29:33 +0200

    draw circles

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var data = [5,11,18,22,7];
 
 var svg = d3.select("body").append("svg")
-    .attr("width","800")
-    .attr("height","600");
+    .attr("width",800)
+    .attr("height",600);
 
 
-svg.selectAll("rect")
+svg.selectAll("circle")
     .data(data)
-    .join('rect')
-    .attr('x',100)
-    .attr('y',function(d,i){
-        return 100+i*20;
+    .join('circle')
+    .attr('cx',function(d,i){
+        return 100+ i*100;
     })
-    .attr('width',function(d,i){
-        return d*20;
-    })
-    .attr('height',10);
-    
+    .attr('cy',100)
+    .attr('r',function(d,i){
+        return d;
+    });
